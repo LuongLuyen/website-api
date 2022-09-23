070804cbbc3a2ef760a78b5767f77c0cf2e93a95
LuongLuyen <91484905+LuongLuyen@users.noreply.github.com>
2022-09-23 12:24:04 +0700

    fix env

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -4,7 +4,16 @@ const filmController ={
     //add film
     addFilm: async (req, res) => {
         const { title,content} = req.body
+        // validation
+        if (!title)
+        return 
+            res.status(400).json()
         try {
+            // Check for existing film
+            const film = await Film.findOne({ title })
+            if (film)
+                return 
+                    res.status(400).json()
             //create film
             const newFilm = new Film({ title, content})
             //film save DB
