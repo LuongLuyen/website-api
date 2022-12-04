7264fa9f4c3ee7cc84d0d094b4072748473e8cc3
LuongLuyen <91484905+LuongLuyen@users.noreply.github.com>
2022-12-04 09:50:47 +0700

    cors #2

--- index.js
+++ index.js
@@ -8,17 +8,16 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 dotenv.config()
-
-app.user('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "X-Requested-With")
-    next()
-})
-
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use('/uploads', express.static('./uploads'))
+
+app.use('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    next()
+})
 
 
 
