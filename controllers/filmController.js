cc49a932078b5c3c1c35661dfb678cf71b48e4ff
LuongLuyen <91484905+LuongLuyen@users.noreply.github.com>
2023-01-07 10:11:19 +0700

    deloy render

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -6,7 +6,7 @@ const filmController ={
     addFilm: async (req, res) => {
         let { name,title,fileName} = req.body
         console.log({name,title,fileName})
-        const port = 'https://luongluyen-film-server.herokuapp.com'
+        const port = 'https://server-97i3.onrender.com'
         video = `${port}/uploads/${fileName}`
         const img =`${port}/uploads/${Math.floor(Math.random() * 10)}.jpg`
         // validation
