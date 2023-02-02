9857e80c2d171d961d8abfbf8bfb5f7354caa310
LuongLuyen <91484905+LuongLuyen@users.noreply.github.com>
2023-02-02 09:34:55 +0700

    Access-Control-Allow-Origin #1

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use('/uploads', express.static('./uploads'))
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://client-97i3.netlify.app, http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Origin', 'https://client-97i3.netlify.app')
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
     res.setHeader('Access-Control-Allow-Credentials', true)
